3451a3f6321b3bcd54192c00e09e0e4191e8e9b3
DanielaNicola <nicola.daniela3103@gmail.com>
2022-10-23 13:12:35 +0300

    define table, MainData

--- app/src/main/java/com/example/findyourdestination/MainActivity.java
+++ app/src/main/java/com/example/findyourdestination/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.findyourdestination;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     //Initialize variable
+    EditText editText;
+    Button btAdd, btReset;
+    RecyclerView recyleView;
+
+    List<MainData> datalist = new ArrayList<>();
+    LinearLayoutManager linearLayoutManager;
+    RoomDB database;
 
 
 
@@ -14,5 +27,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assigne variables
+        editText = findViewById(R.id.edit_text);
+        btAdd = findViewById(R.id.bt_add);
+        btReset = findViewById(R.id.bt_reset);
+        recyleView = findViewById(R.id.recycler_view);
+
     }
 }
